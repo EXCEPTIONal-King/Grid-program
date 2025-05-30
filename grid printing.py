cd6d6bfbee82b12773b31e78e177fcb839e22f25
EXCEPTIONal-King <bencolep@gmail.com>
2025-05-30 00:01:12 -0400

    read full page to pageText and first draft of extracting table componenents

---
+++ grid printing.py
@@ -0,0 +1,14 @@
+def PrintGridFromURL(url):
+
+    return
+
+
+def ReadFile(url):
+    grid = (1,1)
+
+    return grid
+
+
+def PrintGrid(grid):
+
+    return
--- gridprinting.js
+++ gridprinting.js
@@ -3,19 +3,63 @@ function printGridFromURL(url) {
 }
 
 async function readGridFromURL(url) {
-    document = await fetch(url);
-    console.log(document.body);
-    reader = document.body.getReader();
-    line = await reader.read();
+    //read web page
+    google_document = await fetch(url);
+    reader = google_document.body.getReader();
+    var pageText;
+    let line;
+    do {
+        line = await reader.read();
+        if (line.value == undefined) break;
+        for (var i = 0; i < line.value.length; i++) {
+                pageText = pageText + String.fromCharCode(line.value[i]);
+        }
+        console.log(pageText.slice(pageText.length - 20));
+    } while (line.done == false);
+
+
+    console.log(pageText);
+
     console.log(line);
-    var pageText
-    for (var i = 0; i < line.value.length; i++) {
-            pageText = pageText + String.fromCharCode(line.value[i]);
+    //get table
+
+    let max_y = 0;
+    let max_x = 0;
+    let elements = [];
+    let array_index = 0;
+
+    let prev_row_index = 0;
+
+
+
+    while (pageText.indexOf("<tr", prev_row_index) != -1) {
+        prev_row_index = pageText.indexOf("<tr", prev_row_index);
+        let lead_in_string = "<span class=\"c1\">";
+        let index_reader = indexOf(lead_in_string, page_row_index);
+        index_reader = index_reader + lead_in_string.length;
+        let x_coord = pageText.charAt(index_reader);
+        index_reader = pageText.indexOf(lead_in_string, index_reader);
+        index_reader = index_reader + lead_in_string.length;
+        let char = pageText.charAt(index_reader);
+        
+        index_reader = pageText.indexOf(lead_in_string, index_reader);
+        index_reader = index_reader + lead_in_string.length;
+        let y_coord = pageText.charAt(index_reader);
+        
+        elements[array_index++] = [x_coord, char, y_coord];
+        console.log(elements[array_index - 1]);
+
+        prev_row_index += 2; //shift forward to not hit the same "<tr...
+
+
     }
-    console.log(pageText)
+
+
+    //make grid
+
 }
 
-function printGrid(grid) {}
 
 
-printGridFromURL("http://localhost:3005/icecream.html");
+//printGridFromURL("http://localhost:3005/icecream.html");
+printGridFromURL("https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3rha8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub");
